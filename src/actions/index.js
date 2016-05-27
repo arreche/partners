0f91aa26a845500f9bdb7d0ce31cefec5fb97f86
yeraycaballero <yeraycaballero@gmail.com>
2016-05-27 13:03:22 +0100

    make fetch contacts asyncronous

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,12 +4,17 @@ export const FETCH_CONTACTS = 'FETCH_CONTACTS'
 export const FETCH_CONTACTS_ERROR = 'FETCH_CONTACTS_ERROR'
 export const FETCH_CONTACTS_SUCCESS = 'FETCH_CONTACTS_SUCCESS'
 
+
 export function fetchContacts () {
   return (dispatch) => {
     dispatch({ type: FETCH_CONTACTS })
 
-    contactService.loadContacts()
-      .then((contacts) => dispatch({ type: FETCH_CONTACTS_SUCCESS, contacts: contacts }))
-      .catch((error) => dispatch({ type: FETCH_CONTACTS_ERROR, error: error }))
+    return contactService.loadContacts()
+      .then((contacts) => {
+        return dispatch({ type: FETCH_CONTACTS_SUCCESS, contacts: contacts })
+      })
+      .catch((error) => {
+        return dispatch({ type: FETCH_CONTACTS_ERROR, error: error })
+      })
   }
 }
