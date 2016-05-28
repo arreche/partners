4096494c426de44d5dbb668b5d7d1d5782817403
Weydson Lima <thevegancoder@gmail.com>
2016-05-27 17:24:57 -0700

    fixes test case for search contact #11

--- test/reducers/search.test.js
+++ test/reducers/search.test.js
@@ -1,16 +1,41 @@
-import { SEARCH_CONTACTS } from '../../src/actions'
-import reducer from '../../src/reducers/search'
+import { SEARCH_CONTACTS_BY_NAME } from '../../src/actions'
+import reducer from '../../src/reducers/contacts'
 import expect from 'expect'
+import mockContacts from '../../src/stores/mockContacts'
 
 describe('Search reducer', () => {
   it('should return the initial state', () => {
     expect(
-      reducer(undefined, { type: SEARCH_CONTACTS })
-    ).toEqual('')
+      reducer(undefined, {})
+    ).toEqual({contacts:[]})
   })
-  it('should change state', () => {
+  it('should return a state with the result of a search', () => {
     expect(
-      reducer('', { type: SEARCH_CONTACTS, name: 'Ana' })
-    ).toEqual('Ana')
+      reducer({contacts:mockContacts}, { type: SEARCH_CONTACTS_BY_NAME, contacts: {
+        'id': 2,
+        'name': {
+          'first': 'barbara'
+        },
+        'picture': {
+          'large': 'http://api.randomuser.me/portraits/women/74.jpg',
+          'medium': 'http://api.randomuser.me/portraits/med/women/74.jpg',
+          'thumbnail': 'http://api.randomuser.me/portraits/thumb/women/74.jpg'
+        },
+        phone: '123-456-789',
+        email: 'barbara@mail.com'
+      } })
+    ).toEqual({contacts:{
+      'id': 2,
+      'name': {
+        'first': 'barbara'
+      },
+      'picture': {
+        'large': 'http://api.randomuser.me/portraits/women/74.jpg',
+        'medium': 'http://api.randomuser.me/portraits/med/women/74.jpg',
+        'thumbnail': 'http://api.randomuser.me/portraits/thumb/women/74.jpg'
+      },
+      phone: '123-456-789',
+      email: 'barbara@mail.com'
+    }})
   })
 })
